dd54c06c59aaf24690e14198fcff09d98d6620d1
Jerry <zetingyin@gmail.com>
2026-03-08 01:41:43 -0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -276,7 +276,7 @@ with st.sidebar:
     </div>
         
     <div class="sidebar-card">
-        <div style="text-align: left; font-size: 0.85em; color: #64B8FF; font-weight: 600; margin-bottom: 4.5px;">
+        <div style="text-align: center; font-size: 1.1em; color: #64B8FF; font-weight: 600; margin-bottom: 4.5px;">
             上传提示 | TIPS
         </div>
         <div style="text-align: left; font-size: 0.85em; color: #A0AEC0; font-weight: 400; line-height: 1.4;">
