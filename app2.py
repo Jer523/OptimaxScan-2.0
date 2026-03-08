9a7ac3422be3ce92bebff91e99b87d293ae41008
Jerry <zetingyin@gmail.com>
2026-03-08 03:51:58 -0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -288,10 +288,10 @@ with st.sidebar:
         <div style="text-align: left; font-size: 0.9em; color: #A0AEC0; font-weight: 400; line-height: 1.6; margin-left: 14px">
             1. 上传素材 | Upload Files<br>
         </div>
-        <div style="text-align: left; font-size: 0.9em; color: #A0AEC0; font-weight: 400; line-height: 1.6">
+        <div style="text-align: left; font-size: 0.9em; color: #A0AEC0; font-weight: 400; line-height: 1.6; margin-left: 14px">
             2. 智能优化 | Start Refining
         </div>
-            <div style="text-align: left; font-size: 0.7em; color: #A0AEC0; font-weight: 400; line-height: 1.6; letter-spacing: 0.7px">
+            <div style="text-align: left; font-size: 0.7em; color: #A0AEC0; font-weight: 400; line-height: 1.6; letter-spacing: 0.7px; margin-left: 14px">
             分析: 光线/清晰度自适应修复<br>
             Auto-fix light and clarity<br>
             压缩: 体积优化，画质无损<br>
@@ -299,7 +299,7 @@ with st.sidebar:
             排版: 统一 Letter Size 布局<br>
             Standard 8.5×11 Layout
         </div>
-        <div style="text-align: left; font-size: 0.9em; color: #A0AEC0; font-weight: 400; line-height: 1.6">  
+        <div style="text-align: left; font-size: 0.9em; color: #A0AEC0; font-weight: 400; line-height: 1.6; margin-left: 14px">  
             3. 获取黑白扫描 | Download B&W SCANNED PDF
         </div>
     </div>
