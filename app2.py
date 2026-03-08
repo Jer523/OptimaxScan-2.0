92259d3887adc08bcd8dc7b557d51516958d1026
Jerry <zetingyin@gmail.com>
2026-03-08 01:40:46 -0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -259,7 +259,7 @@ st.markdown("""
     color: #64B8FF;
     font-weight: 600;
     text-align: center;
-    margin-bottom: 12px;
+    margin-bottom: 24px;
 }
 </style>
 """, unsafe_allow_html=True)
