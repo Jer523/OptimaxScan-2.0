5b99e9169931d62e53270d6bf576e85c018990d7
Jerry <zetingyin@gmail.com>
2026-03-08 01:36:40 -0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -281,7 +281,6 @@ with st.sidebar:
         <div style="text-align: left; font-size: 0.85em; color: #A0AEC0; font-weight: 400; line-height: 1.4;">
             支持多文件、多格式同步混合上传。
         </div>
-        
     </div>
     ''', unsafe_allow_html=True)
 
